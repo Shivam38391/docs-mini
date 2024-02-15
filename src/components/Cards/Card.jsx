e51e923a388ae2db58ecb5f344df5b3ac1b8d80e
shivam sharma <shivamsharma38391@gmail.com>
2024-02-15 10:47:58 +0530

    added new library frammer motion

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa"
 import { LuDownload } from "react-icons/lu"
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
 
-function Card({ data }) {
+function Card({ data , reference }) {
     return (
         <>
-            <div className=" relative cards w-60 h-72  bg-fuchsia-400 rounded-[48px] px-5 py-10 text-black overflow-hidden">
+            <motion.div drag dragConstraints={reference} whileDrag={{scale:1.1}} dragElastic={.2}  dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }} className=" relative flex-shrink-0 cards w-60 h-72  bg-zinc-800 rounded-[48px] px-5 py-10 text-white overflow-hidden">
 
                 <FaRegFileAlt />
                 <p className='text  leading-tight mt-5'>{data.desc}</p>
 
-                <div className="card-footer absolute bg-slate-200 bottom-0 left-0  w-full   " >
+                <div className="card-footer absolute bg-gray-900 bottom-0 left-0  w-full   " >
                     <div className='flex items-center justify-between py-[7px] px-5 ' >
-                        <h5>{data.filesize}</h5>
+                        <h5 className='   text-white '>{data.filesize}</h5>
                         <span className='w-7 h-6 bg-sky-400 rounded-full flex items-center justify-center'>
                             {data.close ? <IoClose /> : <LuDownload size=".9em" color='#ffffff' />}
                         </span>
@@ -29,8 +30,10 @@ function Card({ data }) {
 
 
                     {data.tag.isOpen && (
-                        <div className="progress-tag w-full text-sm text-center font-semibold py-3 bg-green-600 text-white ">
-                            Download now
+                        <div style={{background: `${data.tag.tagColor}`}} className= {`  progress-tag w-full text-sm text-center font-semibold py-3 text-white`} >
+                            
+                            { data.tag.tagTitle }
+
                         </div>
                     )}
 
@@ -38,7 +41,7 @@ function Card({ data }) {
                 </div>
 
 
-            </div>
+            </motion.div>
 
         </>
     )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Cards/Card'
 
+
 function Foreground() {
+
+    const ref = useRef(null)
 
 
     const data = [
@@ -11,16 +14,36 @@ function Foreground() {
             filesize: ".9mb",
             close: true,
             tag: { isOpen: true, tagTitle: "download Now" ,  tagColor: "green"}
-        }
+        },
+        {
+            desc: "loren hgdhgsD HGDWGH",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "download Now" ,  tagColor: "green"}
+        },
+        {
+            desc: "loren hgdhgsD HGDWGH",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "uploaded" ,  tagColor: "blue"}
+        },
+        {
+            desc: "loren hgdhgsD HGDWGH",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: false, tagTitle: "download Now" ,  tagColor: "red"}
+        },
+
+
     ]
 
     return (
         <>
 
-            <div className='foreground z-[3] fixed w-full h-full ' >
+            <div ref={ref} className='foreground z-[3] fixed w-full h-full flex gap-10 flex-wrap p-8' >
 
                 { data.map((item, index) => (
-                    <Card data= {item}/>
+                    <Card data= {item}  reference= {ref}/>
                 ) ) }
 
             </div>
